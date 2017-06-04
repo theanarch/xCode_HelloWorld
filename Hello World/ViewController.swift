0b77b3b82fd87715d2cdd814b25bac323866378a
Boris Torbica <Obilic@Boriss-Mac-mini.local>
2017-06-04 23:04:43 +0200

    Project 1 - Done

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,19 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate { //UITextFieldDelegate säger att klassen blir ett Delegat
 
+    //MARK: Propertiess
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Delegeringsobjektet(nameTextField) skickar information till delegatet(aktuell klass)
+        nameTextField.delegate = self
     }
     
+    //MARK: Action
+    @IBAction func doneButton(_ sender: UIButton) {
+        mealNameLabel.text = nameTextField.text
+        self.view.endEditing(true) //explained at bottom
+    }
     
-
+    //MARK: UITextFieldDelegate
+    //Func: action of pressing Return button. 
+    // This case: ending first responder status when pressing Return key => hiding keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        return true //spelar än så länge inte roll att det står true/false
+    }
+    
+    //Func: is called when it is asked to resign first responder status.
+    // This case: takes the information and stores it in mealNameLabel
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = nameTextField.text
+    }
+    
+    //       self.view.endEditing(true)
+    //      someTextField.resignFirstResponder()
+    // Difference: the latter one is meant for a specific text field. The first one will terminate any first responder status. Meaning it will go through subviews too => less efficient
 }
-
